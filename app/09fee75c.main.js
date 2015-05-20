7dd5b9d3fbc1e877ff722167a88f5f5f7af159c8
Topher Young <topheryoung@LW-CHS-MAC02.local>
2015-05-19 21:48:46 -0400

    Major bug fixes

--- app/09fee75c.main.js
+++ app/09fee75c.main.js
@@ -67,6 +67,7 @@ $(function() {
 	});
 });
 
+
 /* Enable nav scroll */
 
 $('a[href*=#]:not([href=#])').click(function() {
@@ -83,3 +84,37 @@ $('a[href*=#]:not([href=#])').click(function() {
         }
     }
 });
+
+
+/* Load remote deck information for modal */
+
+$(function () {
+
+    $('.deckButton').on('click', function () {
+
+        var $this = $(this).data('target');
+
+        $('#deckModal').load('../decks/deck1.html ' + $this, function (response, status, xhr) {
+
+            if (status == "success") {
+
+                $($this).modal('show');
+            }
+        });
+    });
+
+});
+
+$(function(){
+         $('.close').click(function(){
+              $('#deckModal').modal('hide');
+          });
+      });
+
+
+/* Pre-loader Initialization */
+// $(window).load(function() { 
+//       $('#status').fadeOut(); 
+//       $('#preloader').delay(350).fadeOut('slow'); 
+//       $('body').delay(350).css({'overflow':'visible'});
+//     })
